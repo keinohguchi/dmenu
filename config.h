248f534e2573e871a721ccb362505086d6faaa71
Kei Nohguchi <kei@nohguchi.com>
2016-04-25 21:45:09 -0700

    config.h: Reverse the color, to make white in the back.

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=16"
+	"monospace:size=16:bold"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
-static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
-static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
+static const char *normbgcolor = "#ffffff"; /* -nb option; normal foreground                 */
+static const char *normfgcolor = "#222222"; /* -nf option; normal background                 */
+static const char *selbgcolor  = "#eeeeee"; /* -sb option; selected foreground               */
+static const char *selfgcolor  = "#005577"; /* -sf option; selected background               */
 static const char *outbgcolor  = "#00ffff";
 static const char *outfgcolor  = "#000000";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
